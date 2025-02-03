a0801b43f3e6f442b5636244875da427d02cdb20
NELSON OMOI <nelsonomoi@NELSONs-Laptop.local>
2025-02-03 08:29:35 +0300

    project scafolding

---
+++ src/main/java/com/oumoi/spring_github_actions/SpringGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.oumoi.spring_github_actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGithubActionsApplication.class, args);
+	}
+
+}
